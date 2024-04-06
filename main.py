8c8a64cc809008998ab480cfc003b399023c2a2f
JoshuaSitumeang <160564706+JoshuaSitumeang@users.noreply.github.com>
2024-04-06 12:55:34 +0000

    Assignment 8 Trial

--- main.py
+++ main.py
@@ -1,2 +1,41 @@
 # Built-in imports
+# Part 1
+def reverse(text):
+    """
+    Reverses a string produced by a word
+    Parameter
+    ---------
+    text: str
+        text to be reversed
 
+    Returns
+    -------
+    str
+        The reversed word
+    """
+    if len(text) == 1:
+        return text
+    else:
+        arrange text reversed
+
+# Part 2
+text = (input("Enter a word = "))
+def is_palindrome(text):
+    """
+    Checks a word to see if it is a palindrome
+    Parameter
+    ---------
+    text: str
+        text to be checked
+
+    Returns
+    -------
+    str
+        The checked word
+    """
+if len(text) = 1:    
+    return text
+elif:
+    check if the word reads the same backwards and forwards
+else:
+    print("This word is not a palindrome.")
